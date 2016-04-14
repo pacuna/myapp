065efe411b0c64be03c445cc8f2fa84811b50271
Pablo Acuña <pabloacuna88@gmail.com>
2016-04-13 23:16:41 -0300

    FIX: another typo

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -1,3 +1,3 @@
 sed -e "s;%BUILD_NUMBER%;0;g" ./deploy/kube/myapp.yaml
 # /usr/bin/kubectl rolling-update myapp-v_%BUILD_NUMBER-1% -f myapp.yml
-/usr/bin/kubectl rolling-update myapp-v2 -f myapp.yml
+/usr/bin/kubectl rolling-update myapp-v2 -f myapp.yaml

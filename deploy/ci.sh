a0719ab14cc93b59b349fed84238069d422c59d5
Pablo Acuña <pabloacuna88@gmail.com>
2016-04-14 20:36:33 -0300

    ADD: stop building if rollling-update fails

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -1,3 +1,13 @@
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ./deploy/kube/myapp.yaml > temp.yaml
+
 /usr/bin/kubectl rolling-update myapp-v${BUILD_NUMBER-1} -f temp.yaml
+
+if [ $? -eq 0 ]
+then
+  echo "Deployment successful"
+else
+  echo "Deployment stopped."
+  exit 0
+fi
+
 rm temp.yaml

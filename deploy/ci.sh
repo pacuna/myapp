2f3a0d930a5069deaeac0cbfa7b0c87051a9d227
Pablo Acuña <pabloacuna88@gmail.com>
2016-04-14 20:53:33 -0300

    TEST

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ./deploy/kube/myapp.yaml > temp.yaml
 
-/usr/bin/kubectl rolling-update myapp-v${BUILD_NUMBER-1} -f temp.yaml
+# /usr/bin/kubectl rolling-update myapp-v${BUILD_NUMBER-1} -f temp.yaml
+/usr/bin/kubectl rolling-update myapp-v12 -f temp.yaml
 
 if [ $? -eq 0 ]
 then

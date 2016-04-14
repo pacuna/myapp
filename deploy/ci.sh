8a246a17fa70fdeaf3062ea5f8395c85542b2f36
Pablo Acuña <pabloacuna88@gmail.com>
2016-04-14 10:49:21 -0300

    ADD: version for deployment

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -1,4 +1,3 @@
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ./deploy/kube/myapp.yaml > temp.yaml
-# /usr/bin/kubectl rolling-update myapp-v_${BUILD_NUMBER-1} -f myapp.yml
-/usr/bin/kubectl rolling-update myapp-v2 -f temp.yaml
+/usr/bin/kubectl rolling-update myapp-v_${BUILD_NUMBER-1} -f temp.yaml
 rm temp.yaml

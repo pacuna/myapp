5b402ff591d5233483503fe782135090fe6cab1c
Pablo Acuña <pabloacuna88@gmail.com>
2016-04-14 20:41:52 -0300

    FIX: add sh to scriptt

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -1,13 +1,6 @@
+#!/bin/sh
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ./deploy/kube/myapp.yaml > temp.yaml
 
 /usr/bin/kubectl rolling-update myapp-v${BUILD_NUMBER-1} -f temp.yaml
-
-if [ $? -eq 0 ]
-then
-  echo "Deployment successful"
-else
-  echo "Deployment stopped."
-  exit 0
-fi
 
 rm temp.yaml
